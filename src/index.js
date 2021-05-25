2a046f04742b2cd72d0393a78bc897ca41e38450
Kenny Yoon <ghun.yoon@gmail.com>
2021-05-25 15:13:37 -0400

    challenge 1 done kenny

--- src/index.js
+++ src/index.js
@@ -2,19 +2,30 @@ console.log('%c HI, Group 6', 'color: firebrick')
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 
+document.addEventListener("DOMContentLoaded", bossDog)
 function bossDog() {
     fetch(imgUrl)
     .then(res => res.json())
-    .then(function(data) {var dogArray = data["message"]})
-    
+    .then(function(data) {
+        let dogArray = data["message"]
+        appendDogs(dogArray)
+    })
 }
 
 //outside function for bossDog (DOM)
 
 function appendDogs(array) {
-    let img = document.createElement("img")
+    
+    
     array.forEach(element => {
-        
+        let div = document.querySelector('#dog-image-container')
+        let img = document.createElement("img")
+        img.src = `${element}`
+        img.alt = `${element}`
+        img.style.height = '200px'
+        img.style.width = '200px'
+        img.style.
+        div.append(img)
     });
 }
 
