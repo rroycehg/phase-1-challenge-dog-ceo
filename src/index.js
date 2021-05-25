50018658dfdf6965882081fc5ae66c91b8fd7bd4
Roli <rroycehg@gmail.com>
2021-05-25 15:00:25 -0400

    Updated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI, Group 6', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function bossDog() {
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then(function(data) {var dogArray = data["message"]})
+    
+}
+
+//outside function for bossDog (DOM)
+
+function appendDogs(array) {
+    let img = document.createElement("img")
+    array.forEach(element => {
+        
+    });
+}
+
